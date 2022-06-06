e50bef40f846896b1b4e99ce269edcdf57c8568c
Ryan Downey <105568842+rjdny@users.noreply.github.com>
2022-06-06 14:43:46 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import { fishCollection } from "../api.js";
+import { fishCollection } from "https://raw.githubusercontent.com/rjdny/Martins-Aquarium/main/api.js";
 
 function dofishy() {
     var html = "";
